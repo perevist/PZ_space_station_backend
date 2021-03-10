e7062d6df48067849eaeefeb6ec6bff5b0f1e162
Patryk Fidrych <perevistp@gmail.com>
2021-03-10 16:27:48 +0100

    Initial commit

---
+++ src/main/java/com/delloite/SpaceStation/SpaceStationApplication.java
@@ -0,0 +1,13 @@
+package com.delloite.SpaceStation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceStationApplication.class, args);
+	}
+
+}
