ecef47fdf9f91c356f2cb417f48423a9dc11d3d0
rootuss <rootuss@op.pl>
2021-03-17 23:03:42 +0100

    add data model

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,24 @@
+DROP TABLE IF EXISTS RESERVATIONS;
+DROP TABLE IF EXISTS USERS;
+
+CREATE TABLE USERS (
+                       id INT AUTO_INCREMENT  PRIMARY KEY,
+                       first_name VARCHAR(45) NOT NULL,
+                       last_name VARCHAR(45) NOT NULL,
+                       phone_number VARCHAR(20) NOT NULL,
+                       email VARCHAR(45) NOT NULL
+);
+
+CREATE TABLE RESERVATIONS (
+                              id INT AUTO_INCREMENT  PRIMARY KEY,
+                              date DATE NOT NULL,
+                              worksite_id INT NOT NULL,
+                              user_id INT NOT NULL,
+                              FOREIGN KEY (user_id)
+                                  REFERENCES USERS(id)
+                                  ON DELETE CASCADE
+);
+
 INSERT INTO USERS (first_name, last_name, phone_number, email)
 VALUES
 ('Adam', 'Nowak', 505334111, 'adamnowak@wp.pl'),
