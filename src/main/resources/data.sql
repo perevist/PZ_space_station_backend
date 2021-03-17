223607cd455e4e18d2f5159445b19e59c3d45e11
rootuss <rootuss@op.pl>
2021-03-17 19:45:09 +0100

    create database and configure it in project

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO USERS (first_name, last_name, phone_number, email)
+VALUES
+('Adam', 'Nowak', 505334111, 'adamnowak@wp.pl'),
+('Adam', 'Kowalski', 505123111, 'adamkowalski@wp.pl'),
+('Ola', 'Biernacka', 888123111, 'olabiernackai@wp.pl'),
+('Michal', 'Kowal', 905123111, 'mk@wp.pl');
+
+INSERT INTO RESERVATIONS (date, worksite_id, user_id)
+VALUES
+('2021-03-21', 2, 1),
+('2021-03-23', 1, 2),
+('2021-03-22', 2, 4),
+('2021-03-22', 3, 3);
+
+
