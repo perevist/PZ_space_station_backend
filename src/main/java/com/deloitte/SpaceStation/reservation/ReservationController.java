b03357585083276ee3f1502f5c68bb9a1909e8fd
Patryk Fidrych <perevistp@gmail.com>
2021-03-18 14:33:51 +0100

    Add API documentation - Swagger2

--- src/main/java/com/deloitte/SpaceStation/reservation/ReservationController.java
+++ src/main/java/com/deloitte/SpaceStation/reservation/ReservationController.java
@@ -1,6 +1,7 @@
 package com.deloitte.SpaceStation.reservation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 @RequestMapping("/api/reservations")
 public class ReservationController {
